19481ac6d3bec4e8126428ff0720b6f4e898f78b
eddbot <edwardsansome@gmail.com>
2023-11-22 21:49:15 +0000

    exercise 4 :)

---
+++ 004_the_thing_namer_3000/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string? a = Console.ReadLine(); // the thing
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string? b = Console.ReadLine(); // a description of the thing
+string c = "Doom"; /* a spicy suffix - removed the 'of' :) */
+string d = "3000"; /* just like Andre */
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+
+// Question - aside from comments how could you make this code clearer?
+
+// Answer - make the variable names more descriptive! (use string interpolation, but we haven't covered that yet :) )
